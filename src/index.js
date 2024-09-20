c0565591dd529c81fe8c68b887d60bcfc1b5078c
alendarov92 <104994198+alendarov92@users.noreply.github.com>
2024-09-20 10:50:00 +0100

    handlebars an d views setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import express from 'express';
-import handlebars from 'express-handlebars'
+import express from "express";
+import handlebars from "express-handlebars";
 
 const app = express();
 
-app.engine('hbs', handlebars.engine({
-    extname:'hbs'
-}));
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
 
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
 
+app.get("/", (req, res) => {
+  res.render('index')
+});
 
-app.get('/', (req, res) => {
-   res.send('Hi there')
-})
-
-
-app.listen(3000, () => console.log('Surver is listening on http://localhost:3000'))
+app.listen(3000, () =>
+  console.log("Surver is listening on http://localhost:3000")
+);
