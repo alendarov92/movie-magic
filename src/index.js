39ff56ca3fdb5ec8981d5e5996e63d96732d6548
alendarov92 <104994198+alendarov92@users.noreply.github.com>
2024-09-22 13:58:26 +0100

    Add body parser midlleware

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", "./src/views");
 
+app.use(express.urlencoded({extended: false}))
+
 app.use(express.static('public'));
 
 app.use(routes)
